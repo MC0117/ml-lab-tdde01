ac96dc3e64fb62417788c10d003268dd77dedacf
MC0117 <manfred.clase@gmail.com>
2025-12-12 01:46:20 +0100

    missed some files in push

--- lab3/assignment1.R
+++ lab3/assignment1.R
@@ -23,8 +23,10 @@ h_space <- 80000
 h_date <- 10
 h_time <- 2
 
-a <- 58.4274 #point to predict
-b <-  14.826
+#a <- 67.91130 remote point
+#b <-  19.60680
+a <-58.8953 #59.8953   17.5935 high frequency point
+b <- 17.5935
 
 date <- as.Date("2013-12-12") #date to predict
 times <- c(
@@ -43,7 +45,9 @@ times <- c(
 
 times <- as.numeric(substr(times, 1, 2))
 
-temp <- vector(length = length(times))
+temp_sum <- vector(length = length(times))     # sum of kernels
+temp_prod <- vector(length = length(times))    # product of kernels
+
 
 #scaled_distance <- function(data, prediction, smoothing) (data-prediction)/h
 gaussian_kernel <- function(u) exp(-u^2)
@@ -70,12 +74,32 @@ for(i in seq_along(times)){
   k_date <- gaussian_kernel(u_date)
   k_time <- gaussian_kernel(u_time)
   
-  w <- k_space + k_date + k_time
-  #w <- k_space * k_date * k_time #product of kernels
+  w_sum <- k_space + k_date + k_time
+  w_prod <- k_space * k_date * k_time #product of kernels
   
-  temp[i] <- sum(w * st_filtered$air_temperature) / sum(w)
-  
+  temp_sum[i] <- sum(w_sum * st_filtered$air_temperature) / sum(w_sum)
+  temp_prod[i] <- sum(w_prod * st_filtered$air_temperature) / sum(w_prod)
+  # 
 }
 
-plot(temp, type="o")
+
+plot(times, temp_sum, type="o", col="blue", pch=16, lwd=2, cex=1.2,
+     xlab="Hour of the day", ylab="Predicted Temperature (°C)",
+     main="Comparison: Sum vs Product Kernels", xaxt="n")
+lines(times, temp_prod, type="o", col="red", pch=17, lwd=2, cex=1.2)
+axis(1, at=times, labels=paste0(times, ":00"))
+grid(nx=NA, ny=NULL, col="lightgray", lty="dotted")
+legend("topright", legend=c("Sum of Kernels", "Product of Kernels"),
+       col=c("blue","red"), pch=c(16,17), lwd=2)
+
+
+# oral defence: comparison of sum vs product of kernels:
+# Using the sum of kernels gives smooth temperature predictions because a measurement
+# gets high weight if it is close in any one of the three dimensions (space, date, or time).
+# Using the product of kernels gives more variable predictions because a measurement
+# only contributes significantly if it is close in all three dimensions simultaneously.
+# Therefore, the sum kernel smooths over gaps in data, while the product kernel emphasizes
+# only the most relevant nearby measurements, leading to sharper peaks and dips.
+
+
 
